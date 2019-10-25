eab93997db4d02d8ed08877a8ac4bd30d5261c3f
Si Hyeong Lee <super2451894@gmail.com>
2019-10-25 22:54:42 +0900

    Add Style
    - Markdown and style

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,11 +1,35 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
 
-const PostLink = props => (
+function getPosts() {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
+  ];
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
@@ -14,10 +38,34 @@ export default function Blog() {
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
       </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: "Arial";
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
     </Layout>
   );
 }
